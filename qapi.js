4f227430924e430ae03f7fc8a1db807d4f2f1a63
Lucas Verra <lucasverra@users.noreply.github.com>
2020-11-29 17:59:13 +0100

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fc39a2db83a2f001f2e4c50",
+				"5fc39a2db83a2f001f2e4c52"
+			]
+		},
+		"5fc39a2db83a2f001f2e4c50": {
+			"id": "5fc39a2db83a2f001f2e4c50",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fc39a2db83a2f001f2e4c52": {
+			"id": "5fc39a2db83a2f001f2e4c52",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,292 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Box, Icon, Input, Button } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { StackItem, Stack, Section, Override, Formspree } from "@quarkly/components";
+import { MdLocationOn, MdEmail, MdPhone } from "react-icons/md";
+import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section
+			padding="140px 0"
+			sm-padding="40px 0"
+			background="linear-gradient(0deg,rgba(0,0,0,.2) 0%,rgba(0,0,0,.2) 100%),--color-dark url(https://images.unsplash.com/photo-1486074051793-e41332bf18fc?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80) 50% 15%/cover"
+			color="--light"
+			font="--base"
+		>
+			<Box>
+				<Text
+					as="h1"
+					margin="0 0 96px 0"
+					text-align="center"
+					letter-spacing="50px"
+					text-transform="uppercase"
+					font="100 132px/1.2 --fontFamily-sans"
+					lg-font="200 42px/1.2 --fontFamily-sans"
+					lg-letter-spacing="20px"
+				>
+					Mary
+					<br />
+					Cornell
+				</Text>
+			</Box>
+			<Stack>
+				<StackItem
+					width="25%"
+					lg-width="50%"
+					sm-width="100%"
+					font="--lead"
+					text-transform="uppercase"
+					letter-spacing="1px"
+				>
+					<Text margin="0px">
+						About me
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						My name is Mary Cornell, born and raised in France.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						I've been working as a professional photographer and videographer for more than 12 years.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						CDS Documentary Essay Prize and Best Photographer of The Year 2019 by Sony World Photography Awards.
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section background="--color-darkL2" padding="64px 0" sm-padding="40px 0">
+			<Stack>
+				<StackItem width="50%" lg-width="100%">
+					<Override
+						slot="StackItemContent"
+						color="--dark"
+						padding="98px 64px"
+						background="--color-light"
+						flex-direction="column"
+					/>
+					<Text
+						as="h4"
+						margin="12px 0"
+						font="--base"
+						color="--grey"
+						letter-spacing="1px"
+						text-transform="uppercase"
+					>
+						About Zanzibar
+					</Text>
+					<Text as="h2" margin="12px 0" font="--headline2" md-font="--headline3">
+						This is one of the world's greatest cultural crossroads where Africa meets the Indian Ocean.
+					</Text>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Stack>
+						<StackItem width="100%" lg-width="33.3%" md-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1575999080555-3f7a698dd8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80) center/cover" />
+						</StackItem>
+						<StackItem width="50%" lg-width="33.3%" md-width="50%" sm-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1577315734214-4b3dec92d9ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80) center/cover" />
+						</StackItem>
+						<StackItem width="50%" lg-width="33.3%" md-width="50%" sm-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1575999502951-4ab25b5ca889?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1568&q=80) center/cover" />
+						</StackItem>
+					</Stack>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section
+			color="--light"
+			padding="100px 0"
+			sm-padding="40px 0"
+			position="relative"
+			background="linear-gradient(0deg,rgba(25, 30, 34, 0.8) 0%,rgba(25, 30, 34, 0.8) 100%),--color-darkL2 url(https://images.unsplash.com/photo-1606654292373-62b5515adfd6?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0MjIzN30&h=2000)"
+		>
+			<Stack>
+				<StackItem width="50%" md-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box
+						padding="0 0 0 64px"
+						sm-padding="64px 0 0 0"
+						margin="32px 0 0 0"
+						max-width="360px"
+						position="relative"
+					>
+						<Icon
+							position="absolute"
+							size="48px"
+							top="0"
+							left="0"
+							category="md"
+							icon={MdLocationOn}
+						/>
+						<Text as="h4" margin="6px 0" font="--base">
+							Visit us
+						</Text>
+						<Text as="p" margin="6px 0" font="--headline3">
+							4998 Hanover Street, New York, 10011
+						</Text>
+					</Box>
+					<Box
+						padding="0 0 0 64px"
+						sm-padding="64px 0 0 0"
+						margin="64px 0 0 0"
+						max-width="360px"
+						position="relative"
+					>
+						<Icon
+							position="absolute"
+							size="48px"
+							top="0"
+							left="0"
+							category="md"
+							icon={MdEmail}
+						/>
+						<Text as="h4" margin="6px 0" font="--base">
+							Email us
+						</Text>
+						<Text as="p" margin="6px 0" font="--headline3">
+							<Link href="mailto:hello@company.com" text-decoration="none" hover-text-decoration="underline" color="--light">
+								hello@company.com
+							</Link>
+						</Text>
+					</Box>
+					<Box padding="0 0 0 64px" margin="64px 0 0 0" max-width="360px" position="relative">
+						<Icon
+							position="absolute"
+							size="48px"
+							top="0"
+							left="0"
+							category="md"
+							icon={MdPhone}
+						/>
+						<Text as="h4" margin="6px 0" font="--base">
+							Call us
+						</Text>
+						<Text as="p" margin="6px 0" font="--headline3">
+							+1 (234) 567-89-00
+							<br />
+							+1 (234) 567-89-00
+						</Text>
+					</Box>
+					<Box
+						padding="0 0 0 64px"
+						sm-padding="0"
+						margin="48px 0"
+						max-width="360px"
+						position="relative"
+						display="flex"
+					>
+						<Icon
+							category="fa"
+							icon={FaFacebookF}
+							width="48px"
+							height="48px"
+							size="24px"
+							margin-right="16px"
+							background="--color-primary"
+							border-radius="50%"
+						/>
+						<Icon
+							category="fa"
+							icon={FaTwitter}
+							width="48px"
+							height="48px"
+							size="24px"
+							margin-right="16px"
+							background="--color-primary"
+							border-radius="50%"
+						/>
+						<Icon
+							category="fa"
+							icon={FaLinkedinIn}
+							width="48px"
+							height="48px"
+							size="24px"
+							margin-right="16px"
+							background="--color-primary"
+							border-radius="50%"
+						/>
+					</Box>
+				</StackItem>
+				<StackItem width="50%" md-width="100%">
+					<Box
+						max-width="360px"
+						padding="56px 48px"
+						margin="0 0 0 auto"
+						md-max-width="100%"
+						background="--color-primary"
+					>
+						<Text as="h3" font="--headline3" margin="0 0 20px 0">
+							Leave us message
+						</Text>
+						<Formspree>
+							<Stack gap="16px">
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Name
+									</Text>
+									<Input width="100%" name="name" />
+								</StackItem>
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Email
+									</Text>
+									<Input width="100%" type="email" name="email" />
+								</StackItem>
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Message
+									</Text>
+									<Input as="textarea" rows="4" width="100%" name="message" />
+								</StackItem>
+								<StackItem width="100%">
+									<Button background="--color-dark">
+										Send
+									</Button>
+								</StackItem>
+							</Stack>
+						</Formspree>
+					</Box>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
